edc45fa6d3373394a7eaa92c4125e7a77023a18f
nurlianihr <nurlianihr@gmail.com>
2016-10-07 01:47:31 +0700

    init commit

---
+++ app/src/main/java/innovation/tower/com/axida/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package innovation.tower.com.axida.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import innovation.tower.com.axida.R;
+
+public class MainActivity extends AppCompatActivity {
+    //Main Program
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/innovation/tower/com/axida/activity/SplashScreen.java
@@ -0,0 +1,8 @@
+package innovation.tower.com.axida.activity;
+
+/**
+ * Created by RnEST-A305 on 07/10/2016.
+ */
+
+public class SplashScreen {
+}
